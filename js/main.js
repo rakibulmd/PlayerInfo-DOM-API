f7aae926269253f53a999b07bac38223438ee373
Rakibul md <rakib245875@gmail.com>
2022-02-26 21:31:56 +0600

    added basic structure and api link created

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const showPlayer = () => {
+	const searchValue = document.getElementById("search-box").value;
+	const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+
+	console.log(url);
+};
